0a8d27329311c28897effd33fe515da4ea35b793
Eric Chang <echang@pared.com>
2020-04-13 13:18:01 -0700

    comment out ImagePicker for now and resolve it later due to different ImagePicker

--- native-package/src/index.js
+++ native-package/src/index.js
@@ -1,23 +1,25 @@
-import { registerNativeHandlers } from 'react-native-activity-feed-core';
-import ImagePicker from 'react-native-image-picker';
+// import ImagePicker from 'react-native-image-picker';
 import { Platform } from 'react-native';
+import { registerNativeHandlers } from 'react-native-activity-feed-core';
 registerNativeHandlers({
   pickImage: () =>
     new Promise((resolve, reject) => {
-      ImagePicker.showImagePicker(null, (response) => {
-        if (response.error) {
-          reject(Error(response.error));
-        }
-        let { uri } = response;
-        if (Platform.OS === 'android') {
-          uri = 'file://' + response.path;
-        }
-
-        resolve({
-          cancelled: response.didCancel,
-          uri,
-        });
-      });
+      // TODO changey: fix ImagePicker, comment this out to get the initial
+      // mvp
+      // ImagePicker.showImagePicker(null, (response) => {
+      //   if (response.error) {
+      //     reject(Error(response.error));
+      //   }
+      //   let { uri } = response;
+      //   if (Platform.OS === 'android') {
+      //     uri = 'file://' + response.path;
+      //   }
+      //
+      //   resolve({
+      //     cancelled: response.didCancel,
+      //     uri,
+      //   });
+      // });
     }),
 });
 
